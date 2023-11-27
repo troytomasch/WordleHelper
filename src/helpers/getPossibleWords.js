41b87379da7ceab9b49a7c80d656664c5d46c621
Troy Tomasch <troyrh6@gmail.com>
2023-11-26 22:09:08 -0500

    Adjusting scoring
    - Returns a top ten list of words

--- src/helpers/getPossibleWords.js
+++ src/helpers/getPossibleWords.js
@@ -83,6 +83,23 @@ const getCommonLetterScore = (word, removeDuplicateLetters) => {
   return score / 1000;
 };
 
+const removeScore = (wordsList, wordToRemove) => {
+  let newList = [];
+  let minScore = 50;
+  let minWord = "";
+  for (let [word, score] of wordsList) {
+    if (word != wordToRemove) {
+      newList.push([word, score]);
+      if (score < minScore) {
+        minScore = score;
+        minWord = word;
+      }
+    }
+  }
+
+  return [newList, minWord, minScore];
+};
+
 /**
  * Returns a score representative of how common the letters are in their given positions in the word
  * @param {*} word - Word to score
@@ -99,8 +116,7 @@ const getCommonLetterScoreWithPosition = (word) => {
 
 /**
  * Returns a list of possible words based on letters and past Wordle answers
- * @param {number} minLetterScore - Minimum letter score it needs to be counted - Default = 11
- * @param {number} minPositionScore - Minimum letter with position score it needs to be counted - Default = 20
+ * @param {number} listLength - The amount of words it should return
  * @param {boolean} removePastWords - Choose whether to remove past Wordle words from the possible words - Default = false
  * @param {string} grayLetters - String of letters to not include - Default = []
  * @param {[[string, [number]]]} yellowLetters - Letters where the position is not known and the positions it's already been seen - Default = []
@@ -108,8 +124,7 @@ const getCommonLetterScoreWithPosition = (word) => {
  * @returns {[string]}
  */
 const getPossibleWords = async (
-  minLetterScore = 11,
-  minPositionScore = 20,
+  listLength = 10,
   removePastWords = false,
   grayLetters = "",
   yellowLetters = "",
@@ -120,8 +135,12 @@ const getPossibleWords = async (
 
   let allWords = await getAllWords();
   let possibleWords = [];
+  let minListScore = 50;
+  let minWord = "";
+
   if (removePastWords) {
     let pastWords = await getPastWords(false);
+
     while (j < pastWords.length && i < allWords.length - 1) {
       i++;
       if (pastWords[j] != allWords[i]) {
@@ -136,17 +155,28 @@ const getPossibleWords = async (
           continue;
         }
         let letterScore = getCommonLetterScore(word);
-        if (letterScore < minLetterScore) {
-          continue;
-        }
         let positionScore = getCommonLetterScoreWithPosition(word);
-        if (positionScore < minPositionScore) {
-          continue;
+        let currentWordScore = Number(
+          ((letterScore * positionScore) / 10).toFixed(3)
+        );
+        if (possibleWords.length < listLength) {
+          if (currentWordScore < minListScore) {
+            minListScore = currentWordScore;
+            minWord = word;
+          }
+          possibleWords.push([word, currentWordScore]);
+        } else {
+          if (currentWordScore > minListScore) {
+            possibleWords.push([word, currentWordScore]);
+            let removedScore = removeScore(possibleWords, minWord);
+            possibleWords = [];
+            for (let word of removedScore[0]) {
+              possibleWords.push(word);
+            }
+            minWord = removedScore[1];
+            minListScore = removedScore[2];
+          }
         }
-        possibleWords.push([
-          word,
-          ((letterScore * positionScore) / 10).toFixed(3),
-        ]);
       } else {
         j++;
       }
@@ -163,20 +193,38 @@ const getPossibleWords = async (
         continue;
       }
       let letterScore = getCommonLetterScore(word);
-      if (letterScore < minLetterScore) {
-        continue;
-      }
       let positionScore = getCommonLetterScoreWithPosition(word);
-      if (positionScore < minPositionScore) {
-        continue;
+      let currentWordScore = Number(
+        ((letterScore * positionScore) / 10).toFixed(3)
+      );
+      if (possibleWords.length < listLength) {
+        if (currentWordScore < minListScore) {
+          minListScore = currentWordScore;
+          minWord = word;
+        }
+        possibleWords.push([word, currentWordScore]);
+      } else {
+        if (currentWordScore > minListScore) {
+          possibleWords.push([word, currentWordScore]);
+          let removedScore = removeScore(possibleWords, minWord);
+          possibleWords = [];
+          for (let word of removedScore[0]) {
+            possibleWords.push(word);
+          }
+          minWord = removedScore[1];
+          minListScore = removedScore[2];
+        }
       }
-      possibleWords.push([
-        word,
-        ((letterScore * positionScore) / 10).toFixed(3),
-      ]);
     }
   }
 
+  possibleWords.sort((a, b) => {
+    if (a[1] > b[1]) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
   return possibleWords;
 };
 
